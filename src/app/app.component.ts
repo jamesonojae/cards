330def7962dd89339c86c053901dd252fef770cd
jamesonojae <jamesonojae@gmail.com>
2020-08-01 10:24:25 +0100

    complete work on card project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  // creating an array of object to be nexted in the card component
+  posts = [
+    {
+    title: 'Neat Tree',
+    imageUrl: 'assets/tree.jpeg',
+    username: 'nature',
+    content: 'I saw this Tree today',
+    },
+    {
+      title: 'Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainLover',
+      content: 'Here is the Mountain i saw today',
+    },
+    {
+      title: 'Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking1222',
+      content: 'I did some biking today',
+    },
+    {
+      title: 'NEW Biking',
+      imageUrl: 'assets/flower.jpeg',
+      username: 'roseFlower',
+      content: 'I did like Rose Flowers',
+    }
+  ];
 }
